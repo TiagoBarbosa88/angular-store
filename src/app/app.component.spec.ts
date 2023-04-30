5b1f8078ef1903005a67ee25f80b1e6daea737ad
TiagoBarbosa88 <sombr43@gmail.com>
2023-04-30 19:35:57 -0300

    chore: renamed files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'travel-agency'`, () => {
+  it(`should have as title 'angular-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('travel-agency');
+    expect(app.title).toEqual('angular-store');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'travel-agency app is running!'
+      'angular-store app is running!'
     );
   });
 });
